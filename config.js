352d197bf77fc5e40e3eea5ad8cac5905963117d
Oliver Dain <oliver@revl.com>
2018-12-22 17:59:14 -0800

    Oni config is looking good!

--- config.js
+++ config.js
@@ -1,4 +1,47 @@
-// Configuration for Oni: https://onivim.github.io
+// Oni config. Symlink to ~/.config/oni/config.js
+
+const increaseFontSize = (oni) => {
+    var currentFontSize = oni.configuration.getValue("editor.fontSize");
+
+    var newFontSize = parseInt(currentFontSize) + 1;
+
+    oni.configuration.setValues(
+        {
+            "editor.fontSize": newFontSize,
+        }
+    );
+};
+
+const decreaseFontSize = (oni) => {
+    var currentFontSize = oni.configuration.getValue("editor.fontSize");
+
+    var newFontSize = parseInt(currentFontSize) - 1;
+    if (newFontSize < 2) {
+       newFontSize = 2;
+    }
+
+    oni.configuration.setValues(
+        {
+            "editor.fontSize": newFontSize,
+        }
+    );
+};
+
+const activate = (oni) => {
+    console.log("config activated")
+   // Increase font size
+   oni.input.bind("<c-=>", () => increaseFontSize(oni));
+   // Decrease font size
+   oni.input.bind("<c-->", () => decreaseFontSize(oni));
+};
+
 module.exports = {
-    "commandline.mode": false
+   activate,
+   "commandline.mode": false,
+   "ui.colorscheme": "slate",
+   "oni.loadInitVim": true,
+   "autoClosingPairs.enabled": false,
+   // UI customizations
+   "ui.animations.enabled": true,
+   "ui.fontSmoothing": "auto",
 }
